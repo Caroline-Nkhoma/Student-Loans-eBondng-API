d68ac65a1f0b443d4070ea20cfa7abba1f9d1571
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-12 18:04:24 +0200

    Create Student DTOs
    Includes Creation, Read and Update DTOs from the CRUD operations (except Delete which does not require a DTO)

---
+++ Student-Loans-eBonder-API/DTOs/StudentCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentLoanseBonderAPI.DTOs;
+
+public class StudentCreateDTO
+{
+	public IFormFile? NationalIdScan { get; set; }
+	public IFormFile? StudentIdScan { get; set; }
+}
---
+++ Student-Loans-eBonder-API/DTOs/StudentReadDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentLoanseBonderAPI.DTOs;
+
+public class StudentReadDTO
+{
+	public int Id { get; set; }
+	public string AccountId { get; set; }
+	public string? NationalIdScan { get; set; }
+	public string? StudentIdScan { get; set; }
+}
