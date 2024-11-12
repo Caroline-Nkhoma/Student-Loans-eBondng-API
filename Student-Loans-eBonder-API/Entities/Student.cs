5aa685f7ea96bb717eda4f807fa0975d0288cfd8
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-12 18:03:35 +0200

    Create Student Entity
    Represents the data related to a student (excluding authentication related data and general user related data which are implemented by IdentityUser and User, respectively)

---
+++ Student-Loans-eBonder-API/Entities/Student.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentLoanseBonderAPI.Entities;
+
+[Table("student")]
+public class Student
+{
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	[Range(1, int.MaxValue)]
+	[Column("student_id")]
+	public int Id { get; set; }
+	[Required]
+	public string AccountId { get; set; }
+	[Required]
+	public IdentityUser Account { get; set; }
+	public string? NationalIdScan { get; set; }
+	public string? StudentIdScan { get; set; }
+}
