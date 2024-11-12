faa18e8189652c48c594afa9b9f2143f7f4c4250
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-12 17:59:39 +0200

    Create User Entity
    Represents the data related to a user (excluding authentication related data which is implemented by IdentityUser)

---
+++ Student-Loans-eBonder-API/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace StudentLoanseBonderAPI.Entities;
+
+[Table("user")]
+public class User
+{
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	[Range(1, int.MaxValue)]
+	[Column("user_id")]
+	public int Id { get; set; }
+	[Required]
+	public string AccountId { get; set; }
+	[Required]
+	public IdentityUser Account {get; set;}
+	public string? Signature { get; set; }
+	public string? ProfilePicture { get; set; }
+}
