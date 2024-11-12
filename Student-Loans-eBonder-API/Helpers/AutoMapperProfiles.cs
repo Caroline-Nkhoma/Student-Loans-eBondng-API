0aef8eb15fd62cb0012aac4f88703b6daa128570
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-12 18:11:27 +0200

    Create AutoMapper Profiles
    - Contains mapping rules between entity and CRU DTOs (CRUD minus the D)
    - Add mapping rules for Student and User
    - Ignore Files when mapping Creation DTOs to Entities
    - Ignore everything when mapping Update DTOs to Entities

---
+++ Student-Loans-eBonder-API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using StudentLoanseBonderAPI.DTOs;
+using StudentLoanseBonderAPI.Entities;
+
+namespace StudentLoanseBonderAPI.Helpers;
+
+public class AutoMapperProfiles : Profile
+{
+	public AutoMapperProfiles()
+	{
+		CreateMap<Student, StudentReadDTO>();
+		CreateMap<StudentCreateDTO, Student>().ForMember(x => x.NationalIdScan, options => options.Ignore()).ForMember(x => x.StudentIdScan, options => options.Ignore());
+		CreateMap<StudentUpdateDTO, Student>().ForAllMembers(options => options.Ignore());
+
+		CreateMap<User, UserReadDTO>();
+		CreateMap<UserCreateDTO, User>().ForMember(x => x.Signature, options => options.Ignore()).ForMember(x => x.ProfilePicture, options => options.Ignore());
+		CreateMap<UserUpdateDTO, User>().ForAllMembers(options => options.Ignore());
+	}
+}
