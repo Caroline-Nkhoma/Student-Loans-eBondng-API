8f4bfc7af973c2a00ace2dd6ee0577e7a58c582b
bsc-com-10-20 <bsc-com-10-20@unima.ac.mw>
2024-11-25 18:32:59 +0100

    Period controller file commit

---
+++ Student-Loans-eBonder-API/DTOs/PeriodDTO.cs
@@ -0,0 +1,8 @@
+namespace StudentLoanseBonderAPI.DTOs
+{
+    public class PeriodDto
+    {
+        public required string StartDate { get; set; }
+        public required string EndDate { get; set; }
+    }
+}
