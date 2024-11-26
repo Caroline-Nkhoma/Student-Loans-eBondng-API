ebfb14cc399692eb1a3cca32e750205c7610c134
Caroline-Nkhoma <167551771+Caroline-Nkhoma@users.noreply.github.com>
2024-11-26 12:40:48 +0200

    userCreate

--- Student-Loans-eBonder-API/DTOs/UserCreateDTO.cs
+++ Student-Loans-eBonder-API/DTOs/UserCreateDTO.cs
@@ -8,11 +8,6 @@ public class UserCreateDTO
 	public string FirstName {get; set;} = string.Empty;
 	public string Surname {get; set;} = string.Empty;
 	public List<string> OtherNames {get; set;} = [];
-	public string FullName
-	{
-		get => $"{FirstName} {string.Join(' ', [.. OtherNames])} {Surname}";
-	}
-	
 	public IFormFile? Signature { get; set; }
 	public IFormFile? ProfilePicture { get; set; }
 }
