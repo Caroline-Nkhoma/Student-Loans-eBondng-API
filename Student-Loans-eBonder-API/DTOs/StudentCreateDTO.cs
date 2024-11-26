cf9d79a4083a5a7d592832d0eaf938943b6a3e53
Caroline-Nkhoma <167551771+Caroline-Nkhoma@users.noreply.github.com>
2024-11-26 12:17:51 +0200

    studentCreateDto.cs

--- Student-Loans-eBonder-API/DTOs/StudentCreateDTO.cs
+++ Student-Loans-eBonder-API/DTOs/StudentCreateDTO.cs
@@ -4,6 +4,25 @@ namespace StudentLoanseBonderAPI.DTOs;
 
 public class StudentCreateDTO
 {
+	public DateOnly? DateOfBirth {get; set;}
+	public string? Sex {get; set;} = string.Empty;
+	public string? PostalAddress {get; set;} = string.Empty;
+	public string? HomeVillage {get; set;} = string.Empty;
+	public string? TraditionalAuthority {get; set;} = string.Empty;
+	public string? District {get; set;} = string.Empty;
+	[Length(minimumLength: 6, maximumLength: 16)]
+	public string? PhoneNumber {get; set;}
+	public string? NationalIdNumber {get; set;}
+	public string? BankName {get; set;} = string.Empty;
+	public string? BranchName {get; set;} = string.Empty;
+	public string? BankAccountName {get; set;} = string.Empty;
+	public string? BankAccountNumber {get; set;}
+	public string? InstitutionName {get; set;} = string.Empty;
+	public string? ProgrammeOfStudy {get; set;} = string.Empty;
+	public string? RegistrationNumber {get; set;}
+	public string? AcademicYear {get; set;} = string.Empty;
+	[Range(minimum: 1, maximum: 6)]
+	public int? YearOfStudy {get; set;}
 	public IFormFile? NationalIdScan { get; set; }
 	public IFormFile? StudentIdScan { get; set; }
 }
