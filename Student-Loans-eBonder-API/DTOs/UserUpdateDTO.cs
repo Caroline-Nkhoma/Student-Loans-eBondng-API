330b25b52fd263113bd3c14c3f97c0faf3b823bd
Caroline-Nkhoma <167551771+Caroline-Nkhoma@users.noreply.github.com>
2024-11-26 12:41:21 +0200

    userUpdateDTO.cs

--- Student-Loans-eBonder-API/DTOs/UserUpdateDTO.cs
+++ Student-Loans-eBonder-API/DTOs/UserUpdateDTO.cs
@@ -2,6 +2,10 @@
 
 public class UserUpdateDTO
 {
+	
+	public string FirstName {get; set;} = string.Empty;
+	public string Surname {get; set;} = string.Empty;
+	public List<string> OtherNames {get; set;} = [];
 	public IFormFile? Signature { get; set; }
 	public IFormFile? ProfilePicture { get; set; }
 }
