3089df09fa6c68beed50bc860a6dc99019dc9fb8
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-26 07:09:47 +0200

    Create PaginationDTO
    - Adds paged-pagination
    - Pages are 1-based (NOT 0-based)
    - Has a maximum of 50 records per page
    - By default is on first page and has the maximum amount of records per page

---
+++ Student-Loans-eBonder-API/DTOs/PaginationDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentLoanseBonderAPI.DTOs;
+
+public class PaginationDTO
+{
+    public int Page { get; set; } = 1;
+    private readonly int _maxRecordsPerPage = 50;
+
+	private int _recordsPerPage;
+
+	public PaginationDTO()
+	{
+		_recordsPerPage = _maxRecordsPerPage;
+	}
+
+	public int RecordsPerPage
+	{
+		get => _recordsPerPage;
+		set
+		{
+			_recordsPerPage = value > _maxRecordsPerPage ? _maxRecordsPerPage : value;
+		}
+	}
+
+}
