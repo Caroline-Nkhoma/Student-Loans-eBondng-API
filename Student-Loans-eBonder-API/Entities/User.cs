a1bf61ab78dbd01f4b91b52fb45bb1a01dfe72cf
Caroline-Nkhoma <167551771+Caroline-Nkhoma@users.noreply.github.com>
2024-11-26 12:40:36 +0200

    user.cs

--- Student-Loans-eBonder-API/Entities/User.cs
+++ Student-Loans-eBonder-API/Entities/User.cs
@@ -19,10 +19,6 @@ public class User
 	public string FirstName {get; set;} = string.Empty;
 	public string Surname {get; set;} = string.Empty;
 	public List<string> OtherNames {get; set;} = [];
-	public string FullName
-	{
-		get => $"{FirstName} {string.Join(' ', [.. OtherNames])} {Surname}";
-	}
 	public string? Signature { get; set; }
 	public string? ProfilePicture { get; set; }
 }
