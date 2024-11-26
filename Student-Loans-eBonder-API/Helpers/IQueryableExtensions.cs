c856e79279c67c612a72b151bb26599e7e9b2e41
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-26 07:22:42 +0200

    Create IQueryable extensions class
    - Conatins extension functions on generic IQueryables
    - Add Paginate extension function to paginate the records in the IQueryable

---
+++ Student-Loans-eBonder-API/Helpers/IQueryableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using StudentLoanseBonderAPI.DTOs;
+
+namespace StudentLoanseBonderAPI.Helpers;
+
+public static class IQueryableExtensions
+{
+	public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, PaginationDTO paginationDTO)
+	{
+		return queryable.Skip((paginationDTO.Page - 1) * paginationDTO.RecordsPerPage).Take(paginationDTO.RecordsPerPage);
+	}
+}
