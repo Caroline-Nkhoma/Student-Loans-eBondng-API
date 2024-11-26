c05afd5a3c540635b7bd909f36f70d3a9145df4c
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-26 07:26:57 +0200

    Create HttpContext extensions class
    - Conatins extension functions on a given HttpContext
    - Add InsertParametersPaginationInHeader extension function to insert pagination parameters [namely, the total number of records] into HTTP Response Headers

---
+++ Student-Loans-eBonder-API/Helpers/HttpContextExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentLoanseBonderAPI.Helpers;
+
+public static class HttpContextExtensions
+{
+	public static async Task InsertParametersPaginationInHeader<T>(this HttpContext httpContext, IQueryable<T> queryable)
+	{
+		if (httpContext == null) throw new ArgumentNullException(nameof(httpContext));
+
+		int count = await queryable.CountAsync();
+		httpContext.Response.Headers.Add("total_amount_of_records", count.ToString());
+	}
+}
