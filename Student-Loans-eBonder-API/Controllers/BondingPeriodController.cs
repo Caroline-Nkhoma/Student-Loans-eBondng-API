7ff753a6aa71dcd68a907505995c05cf2bba1c4b
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-30 18:25:46 +0200

    Add Authorization of BondingPeriodContoller
    - Also use file-based namespace

--- Student-Loans-eBonder-API/Controllers/BondingPeriodController.cs
+++ Student-Loans-eBonder-API/Controllers/BondingPeriodController.cs
@@ -1,65 +1,67 @@
-using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StudentLoanseBonderAPI.DTOs;
-using StudentLoanseBonderAPI.Entities;
 using StudentLoanseBonderAPI.Services;
 
-namespace StudentLoanseBonderAPI.Controllers
+namespace StudentLoanseBonderAPI.Controllers;
+
+[ApiController]
+[Route("api/institutions/{institutionId}/bonding-periods")]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class BondingPeriodController : ControllerBase
 {
-    [ApiController]
-    [Route("api/institutions/{institutionId}/bonding-periods")]
-    public class BondingPeriodController : ControllerBase
-    {
-        private readonly BondingPeriodService _bondingPeriodService;
+	private readonly BondingPeriodService _bondingPeriodService;
 
-        public BondingPeriodController(BondingPeriodService bondingPeriodService)
-        {
-            _bondingPeriodService = bondingPeriodService;
-		}
+	public BondingPeriodController(BondingPeriodService bondingPeriodService)
+	{
+		_bondingPeriodService = bondingPeriodService;
+	}
 
-		[HttpGet]
-		public async Task<ActionResult<BondingPeriodReadDTO>> GetBondingPeriod([FromRoute] string institutionId)
+	[HttpGet]
+	public async Task<ActionResult<BondingPeriodReadDTO>> GetBondingPeriod([FromRoute] string institutionId)
+	{
+		var bondingPeriod = await _bondingPeriodService.FindOneByInstitutionId(institutionId);
+
+		if (bondingPeriod != null)
 		{
-			var bondingPeriod = await _bondingPeriodService.FindOneByInstitutionId(institutionId);
-
-			if (bondingPeriod != null)
-			{
-				return bondingPeriod;
-			}
-			else
-			{
-				return NotFound();
-			}
+			return bondingPeriod;
 		}
-
-		[HttpPost]
-        public async Task<ActionResult> SetBondingPeriod([FromRoute] string institutionId, [FromBody] BondingPeriodCreateDTO bondingPeriodCreateDTO)
-        {
-            var created = await _bondingPeriodService.Create(institutionId, bondingPeriodCreateDTO);
-
-			if (created)
-			{
-				return Created();
-			}
-			else
-			{
-				return BadRequest();
-			}
-		}
-
-		[HttpDelete]
-		public async Task<ActionResult> DeleteBondingPeriod([FromRoute] string institutionId)
+		else
 		{
-			var deleted = await _bondingPeriodService.Delete(institutionId);
+			return NotFound();
+		}
+	}
 
-			if (deleted)
-			{
-				return NoContent();
-			}
-			else
-			{
-				return NotFound();
-			}
+	[HttpPost]
+	[Authorize(Roles = "LoansBoardOfficial")]
+	public async Task<ActionResult> SetBondingPeriod([FromRoute] string institutionId, [FromBody] BondingPeriodCreateDTO bondingPeriodCreateDTO)
+	{
+		var created = await _bondingPeriodService.Create(institutionId, bondingPeriodCreateDTO);
+
+		if (created)
+		{
+			return Created();
+		}
+		else
+		{
+			return BadRequest();
+		}
+	}
+
+	[HttpDelete]
+	[Authorize(Roles = "LoansBoardOfficial,SystemAdmin")]
+	public async Task<ActionResult> DeleteBondingPeriod([FromRoute] string institutionId)
+	{
+		var deleted = await _bondingPeriodService.Delete(institutionId);
+
+		if (deleted)
+		{
+			return NoContent();
+		}
+		else
+		{
+			return NotFound();
 		}
 	}
 }
