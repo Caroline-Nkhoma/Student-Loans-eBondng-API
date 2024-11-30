34f2808706467224a901e302a09a099f3eacf31b
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-30 18:07:09 +0200

    Create SMTPServerEmailService
    Implements IEmailService using a SMTP Server

---
+++ Student-Loans-eBonder-API/Services/SMTPServerEmailService.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Mail;
+
+namespace StudentLoanseBonderAPI.Services;
+
+public class SMTPServerEmailService : IEmailService
+{
+	private readonly ILogger<SMTPServerEmailService> _logger;
+	private readonly SmtpClient _smtpClient;
+	private readonly string _senderEmailAddress;
+
+	public SMTPServerEmailService(ILogger<SMTPServerEmailService> logger, SmtpClient smtpClient, IConfiguration configuration)
+	{
+		_logger = logger;
+		_smtpClient = smtpClient;
+		_senderEmailAddress = configuration.GetValue<string>("EmailService:SenderEmailAddress");
+	}
+
+	public async Task<bool> SendEmailAsync(string recipients, string subject, string body)
+	{
+		_logger.LogInformation($"Sending email about '{subject}' to {recipients}");
+		MailMessage message = new()
+		{
+			From = new MailAddress(_senderEmailAddress),
+			Subject = subject,
+			IsBodyHtml = true,
+			Body = body
+		};
+		message.To.Add(recipients);
+
+		try
+		{
+			await _smtpClient.SendMailAsync(message);
+			_logger.LogInformation($"Successfully sent email about '{subject}' to {recipients}");
+			return true;
+		}
+		catch (Exception e)
+		{
+			_logger.LogError($"Failed to send email about '{subject}' to {recipients}");
+			_logger.LogError(e.Message);
+			return false;
+		}
+	}
+}
